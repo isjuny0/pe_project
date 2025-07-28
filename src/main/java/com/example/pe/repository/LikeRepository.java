f01eaf8347bbab53efa02a6f6e9f770cd3a53ba1
juny0 <isjuny0k@gmail.com>
2025-07-28 23:23:52 +0900

    feat: repository 추가

---
+++ src/main/java/com/example/pe/repository/LikeRepository.java
@@ -0,0 +1,14 @@
+package com.example.pe.repository;
+
+import com.example.pe.entity.Like;
+import com.example.pe.entity.Photo;
+import com.example.pe.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    int countByPhoto(Photo photo);
+    Optional<Like> findByPhotoAndUser(Photo photo, User user);
+    boolean existsByPhotoAndUser(Photo photo, User user);
+}
---
+++ src/main/java/com/example/pe/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.pe.repository;
+
+import com.example.pe.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+}
